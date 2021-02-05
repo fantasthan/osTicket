82d177ecd08b692962f8336a0a5be08aa06d6a72
fantasthan <75408464+fantasthan@users.noreply.github.com>
2021-02-05 12:56:11 +0900

    Update index.php

--- index.php
+++ index.php
@@ -13,9 +13,9 @@
 
     vim: expandtab sw=4 ts=4 sts=4:
 **********************************************************************/
-/*
-require('client.inc.php');
 
+require('client.inc.php');
+/*
 require_once INCLUDE_DIR . 'class.page.php';
 
 $section = 'home';
