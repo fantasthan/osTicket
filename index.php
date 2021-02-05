c288a8582e3e18c6341992f00f2e1162c6bd5311
fantasthan <75408464+fantasthan@users.noreply.github.com>
2021-02-05 12:54:53 +0900

    Update index.php

--- index.php
+++ index.php
@@ -13,9 +13,9 @@
 
     vim: expandtab sw=4 ts=4 sts=4:
 **********************************************************************/
-
-require('client.inc.php');
 /*
+require('client.inc.php');
+
 require_once INCLUDE_DIR . 'class.page.php';
 
 $section = 'home';
@@ -80,4 +80,5 @@ if ($cats->all()) { ?>
 </div>
 </div>
 */
-<?php require(CLIENTINC_DIR.'footer.inc.php'); ?>
+//<?php require(CLIENTINC_DIR.'footer.inc.php'); ?>
+?>
